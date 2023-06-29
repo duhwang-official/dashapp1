be7f83e4db3175aa93fffe833b6e43a43b54f0ed
Dong-Uk Hwang <123041175+duhwang-official@users.noreply.github.com>
2023-06-29 11:48:21 +0900

    Initial commit

---
+++ app.jl
@@ -0,0 +1,15 @@
+using Dash #, DashHtmlComponents, DashCoreComponents
+
+app = dash()
+
+app.layout = html_div() do
+    html_h1("Hello, World!")
+end
+
+# run_server(app, "0.0.0.0", debug=true)
+
+# for Heroku
+
+port = parse(Int64, ENV["PORT"])
+
+run_server(app,"0.0.0.0", port)
